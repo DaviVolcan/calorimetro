9d550b196de142a732d1043088eb4c8d4272ca17
Davi Volcan <davi.volcan2@gmail.com>
2023-11-10 14:38:02 -0300

    commit inicial, nesse copdigo já conseguimos verificar o funcionamento da celula de carga, falta o ds18b20, que provavelmente nao está  funcionando devido a falta de um resistor de pullpup

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include "HX711.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 15;
+const int LOADCELL_SCK_PIN = 16;
+
+const int ONE_WIRE_BUS = 17; // Pin para o sensor DS18B20
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(57600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  sensors.begin();
+}
+
+void loop() {
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  sensors.requestTemperatures(); // Solicita a leitura da temperatura ao sensor DS18B20
+  float temperatureC = sensors.getTempCByIndex(0); // Obtém a temperatura em graus Celsius
+
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" °C");
+
+  delay(1000);
+}
